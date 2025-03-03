050c6b9f4994dd447324f5a39a083351f727de26
sivarepo24 <sivaravichandran11@gmail.com>
2025-03-03 07:14:02 +0530

    reslove the error 3

--- test_app.py
+++ test_app.py
@@ -1,11 +1,9 @@
 import unittest
-from app import hello
+from app import hello  # Ensure that the `app.py` is in the same directory or accessible
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), "welcome to jenkins")
-
-
+        self.assertEqual(hello(), "welcome to jenkins")  # Fixed the typo here
 
 if __name__ == "__main__":
     unittest.main()
