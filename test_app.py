30346b878e068c10ca4e988a9e962f18c2414089
sivarepo24 <sivaravichandran11@gmail.com>
2025-03-20 06:44:53 +0530

    server deployment

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
 import unittest
-from app import hello  # Ensure that the `app.py` is in the same directory or accessible
+from app import home  # Ensure that the `app.py` is in the same directory or accessible
 
 class TestApp(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), "Hello, Dockerized World!")  # Fixed the typo here
+        self.assertEqual(home(), "Hello, Dockerized World!")  # Fixed the typo here
 
 if __name__ == "__main__":
     unittest.main()
